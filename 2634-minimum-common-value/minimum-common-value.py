6df8b38cc145bc612673093c4fdc0776444bbb7d
Adil Muhammad Pervez <76257781+Adilmp@users.noreply.github.com>
2025-05-13 15:39:56 +0500

    Time: 11 ms (85.23%) | Memory: 30.3 MB (31.06%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def getCommon(self, nums1, nums2):
+        seen = set(nums1)  # store all nums1 in a set
+        for num in nums2:  # since nums2 is sorted, you'll find the smallest match first
+            if num in seen:
+                return num
+        return -1
+#O(n) = Time complexity
